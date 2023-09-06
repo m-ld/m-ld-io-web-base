143e65623102915cdb51683b11bef4298a6f2c19
George Svarovsky <gsvarovsky@users.noreply.github.com>
2023-09-06 14:46:14 +0100

    Switching website builds from browserify (requires babel for latest ES), to tsup

--- build/index.js
+++ build/index.js
@@ -1,11 +1,14 @@
-const { join, dirname } = require('path');
+const {
+  join,
+  dirname,
+  basename
+} = require('path');
 const { promisify } = require('util');
-const browserify = require('browserify');
-const { minify } = require('terser');
-const concat = require('concat-stream');
 const sass = require('node-sass');
 const postcss = require('postcss');
-const envify = require('envify');
+const { build } = require('tsup');
+const { resolveNode } = require('@m-ld/io-js-build/esbuild');
+const { readFile } = require('fs/promises');
 
 function packageDir(module, localRequire) {
   return dirname((localRequire || require).resolve(module + '/package.json'));
@@ -15,7 +18,9 @@ exports.packageDir = packageDir;
 exports.default11tyConfig = function (eleventy, config) {
   config ??= {};
   (config.dir ??= {}).input ??= 'src';
-  (config.templateFormats ??= []).push('liquid', 'html', 'svg', 'png', 'md', '11ty.js', '11ty.cjs');
+  (config.templateFormats ??= []).push(
+    'liquid', 'html', 'svg', 'png', 'md', '11ty.js', '11ty.cjs'
+  );
   if (config.fontawesome !== false) {
     eleventy.addPassthroughCopy({
       [join(packageDir('@fortawesome/fontawesome-free'), 'webfonts')]: 'webfonts'
@@ -28,20 +33,33 @@ exports.default11tyConfig = function (eleventy, config) {
   eleventy.setBrowserSyncConfig({ ghostMode: false });
   eleventy.setLiquidOptions({ dynamicPartials: true });
   return config;
-}
+};
 
-exports.renderTs = async function ({ tsPath, tsConfig }) {
+/**
+ * Creates browser bundle for given TypeScript file. If process.env.NODE_ENV is
+ * development, includes source maps; otherwise, minifies.
+ *
+ * @param {string} tsPath typescript file path
+ * @param {string[]} shims any wanted resolvable non-core module
+ * @param {string[]} [envVars] environment variables to substitute in the output
+ * @returns {Promise<Buffer | string>} bundle file
+ */
+exports.renderTs = async function (
+  { tsPath, shims, envVars }
+) {
   const dev = process.env.NODE_ENV === 'development';
-  const b = browserify(tsPath, { debug: dev, ignoreMissing: true })
-    .plugin('tsify', tsConfig.compilerOptions)
-    .transform(envify, { global: true });
-
-  return new Promise(async (resolve, reject) => {
-    const demoStream = b.bundle();
-    demoStream.on('error', reject);
-    demoStream.pipe(concat(resolve));
-  }).then(async js => dev ? js : (await minify(js.toString())).code);
-}
+  await build({
+    entry: [tsPath],
+    format: 'iife',
+    platform: 'browser',
+    esbuildPlugins: [resolveNode(shims, envVars)],
+    sourcemap: dev ? 'inline' : false,
+    noExternal: [/(.*)/], // Bundle all dependencies
+    minify: !dev
+  });
+  // Will now have built to ./dist
+  return readFile(join('./dist', `${basename(tsPath, '.ts')}.global.js`));
+};
 
 exports.renderScss = async function ({ scssPath }) {
   const { css } = await promisify(sass.render)({
@@ -54,4 +72,4 @@ exports.renderScss = async function ({ scssPath }) {
   const result = await postcss([require('autoprefixer')]).process(css);
   result.warnings().forEach(console.warn);
   return result.css;
-}
+};
